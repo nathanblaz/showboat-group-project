62101352679b7521cc23d0b90927122be9219a8d
Melonnie <melonnie13@gmail.com>
2021-06-21 19:29:48 -0500

    trying to checkout main

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
-from .models import db
+from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 
