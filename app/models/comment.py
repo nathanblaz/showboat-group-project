97acdbacb3e04f277049f471c3bd873f5b44f027
Melonnie <melonnie13@gmail.com>
2021-06-22 15:48:36 -0500

    added two columns to comment model

--- app/models/comment.py
+++ app/models/comment.py
@@ -6,6 +6,8 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     comment = db.Column(db.Text)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
     photo_id = db.Column(db.Integer, db.ForeignKey('photos.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     photos = db.relationship("Photo", back_populates="comments")
