c2afe40c6003d518839dbce4356e49180f08007b
Sarah <jacobs.b.sarah@gmail.com>
2021-06-21 22:30:44 -0700

    tags as links

--- react-app/src/components/TagSidebar.js
+++ react-app/src/components/TagSidebar.js
@@ -9,12 +9,12 @@ const TagSidebar = () => {
         <div className="tag-sidebar">
             {/* for each tag in Tag.query.all() */}
             {/* <div className="tag-sidebar-tag"><NavLink>Tag1</NavLink></div> */}
-            <div className="tag-sidebar-tag">Tag</div>
-            <div>Tag</div>
-            <div>Example Tag</div>
-            <div>Another Long Tag</div>
-            <div>Activities</div>
-            <div>Hobbies</div>
+            <div className="tag-sidebar-tag"><a href="#">Tag</a></div>
+            <div className="tag-sidebar-tag"><a href="#">Tag</a></div>
+            <div className="tag-sidebar-tag"><a href="#">Example Tag</a></div>
+            <div className="tag-sidebar-tag"><a href="#">Another Long Tag</a></div>
+            <div className="tag-sidebar-tag"><a href="#">Activities</a></div>
+            <div className="tag-sidebar-tag"><a href="#">Hobbies</a></div>
         </div>
     )
 }
