8cc0536fcf52db8f99dcc04abc180f9e25b75976
Nick Kury <nickkury12@gmail.com>
2021-06-23 12:48:14 -0700

    renders comments

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -8,5 +8,18 @@ comment_routes = Blueprint('comments', __name__)
 
 @comment_routes.route("")
 def get_photo_comments():
-    comments = Comment.query.order_by(Comment.created_at.desc()).all()
+    comments = Comment.query.filter_by(Comment.photo_id).all()
     return {'comments': [comment.to_dict() for comment in comments]}
+
+
+@comment_routes.route("", methods={"POST"})
+@login_required
+def post_comment():
+    new_comment = Comment(
+        comment=request.form['comment'],
+        photo_id=photo.id,
+        user_id=current_user.id
+    )
+    db.session.add(new_comment)
+    db.session.commit()
+    return new_comment.to_dict()
--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -4,6 +4,8 @@ from app.models import db, Comment
 def seed_comments():
     comment1 = Comment(
         comment='Best concert ever!',
+        photo_id=1,
+        user_id=1
         
         )
 
