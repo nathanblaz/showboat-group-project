ea8dd23dfe01f1cf8ddba057034212c6399cbd0d
Melonnie <melonnie13@gmail.com>
2021-06-24 15:25:55 -0500

    reverted sidebar

--- react-app/src/components/TagSidebar.js
+++ react-app/src/components/TagSidebar.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import {renderPhotoTags} from '../../src/store/tag';
 
 const TagSidebar = () => {
+    //const tags = useSelector(state for tags?)
     const dispatch = useDispatch();
     const tags = useSelector(state => Object.values(state.tagReducer));
 
@@ -13,18 +14,13 @@ const TagSidebar = () => {
 
     return (
         <div className="tag-sidebar">
-            {/* for each tag in Tag.query.all() */}
             {/* <div className="tag-sidebar-tag"><NavLink>Tag1</NavLink></div> */}
-            {tags.map(tag =>
-            <div className="tag-sidebar-tag" key={tag.id}><a href="#">{`${tag?.name}`}</a>
+            <h2>Explore Photos By Tag</h2>
+            {tags?.map(tag =>
+            <div className="tag-sidebar-tag" key={tag?.id}><a href="#">{`${tag?.name}`}</a>
             </div>
                 )}
-            {/* <div className="tag-sidebar-tag"><a href="#">Tag</a></div>
-            <div className="tag-sidebar-tag"><a href="#">Example Tag</a></div>
-            <div className="tag-sidebar-tag"><a href="#">Another Long Tag</a></div>
-            <div className="tag-sidebar-tag"><a href="#">Activities</a></div>
-            <div className="tag-sidebar-tag"><a href="#">Hobbies</a></div> */}
-            <h2></h2>
+
         </div>
     )
 }
