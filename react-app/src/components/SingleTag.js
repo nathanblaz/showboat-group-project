4f593196e0884bbd95bba08a00ab80cf290c151c
Melonnie <melonnie13@gmail.com>
2021-07-27 16:59:38 -0500

    styling on single tag page/ working tag update

--- react-app/src/components/SingleTag.js
+++ react-app/src/components/SingleTag.js
@@ -4,7 +4,6 @@ import { useParams } from 'react-router';
 import { getOneTag } from '../store/tag';
 import DeleteATag from './DeleteATag';
 import UpdateTag from './UpdateTag';
-import TagSidebar from './TagSidebar';
 
 const SingleTag = () => {
     const dispatch = useDispatch();
@@ -30,7 +29,6 @@ const SingleTag = () => {
             </div>
             : null
     }
-        {/* <TagSidebar /> */}
         </div>
     )
 }
